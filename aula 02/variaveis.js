3e3b2fbcb2f6a800f07585844f6dd1e9bb1e5929
Sarah Rufino <sarahrufinoreis@gmail.com>
2024-02-29 08:35:18 -0300

    aula 02 sobre variaveis

---
+++ aula 02/variaveis.js
@@ -0,0 +1,23 @@
+var numero = 2;
+let texto = "Dentro da hilux";
+const hilux = "ela movimenta no beat do tuts tuts";
+
+// dentro da hilux ela movimenta no beat do tuts tuts (ponto e vírgula é opcional) ♥
+
+console.log("primeiro numero e ", numero);
+
+//concatenar com virgula garante espaco
+
+numero = 13;
+
+console.log("o numero agora e " + numero);
+
+texto = 69;
+
+console.log(texto+numero);
+
+//concatenar com o + 2 vai acabar somando
+
+console.log (hilux)
+
+//constantes nao podem ter novos valores assinalados a ela
